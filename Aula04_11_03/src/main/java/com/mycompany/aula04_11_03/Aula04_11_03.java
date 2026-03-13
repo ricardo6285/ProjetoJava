33f8e04cd84bc613b81c033ea03c2ee46d7c8950
ricardo6285 <samucabrindes@gmail.com>
2026-03-13 12:42:50 -0300

    projeto java

---
+++ Aula04_11_03/src/main/java/com/mycompany/aula04_11_03/Aula04_11_03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula04_11_03;
+
+/**
+ *
+ * @author USER
+ */
+public class Aula04_11_03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+
+
