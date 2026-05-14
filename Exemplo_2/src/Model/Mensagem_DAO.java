b94fb21498e273568e8aff707581426aca634d6d
Ricardo Rodrigues da Silva <samucabrindes@gmail.com>
2026-05-13 21:00:41 -0300

    Add files via upload

---
+++ Exemplo_2/src/Model/Mensagem_DAO.java
@@ -0,0 +1,14 @@
+
+package Model;
+
+import javax.swing.JOptionPane;
+
+
+public class Mensagem_DAO {
+    public static void chama(){
+        JOptionPane.showMessageDialog(null, "Cadastro de Cliente");
+    }
+    public static void sair(){
+        System.exit(0);
+    }
+}
